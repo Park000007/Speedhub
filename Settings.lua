dbd02fe40608b01ed82bf506a2d4d49ff81c8b80
PARKHUBSHOP <0924011344aa@gmail.com>
2025-01-15 13:32:04 +0700

    Update Settings.lua

--- Settings.lua
+++ Settings.lua
@@ -1,7 +1,7 @@
 local _env = getgenv and getgenv() or {}
 
 local Ver = "Version: 3.7.0"
-local Dis = "https://discord.gg/mMCseE7e3k"
+local Dis = "https://is.gd/YUADB1"
 
 _env.Discord = Dis
 _env.Version = Ver
